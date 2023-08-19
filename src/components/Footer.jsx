860488a117e0eacc5f44c682770b28d2448ab757
Gabriel Omane - Yeboah <gabrielomane@outlook.com>
2023-08-19 20:54:04 +0000

    Added condition to render completed message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,17 @@ import React from "react";
 const Footer = ({ items }) => {
   const packedList = items.length;
   const numberPacked = items.filter((item) => item.packed).length;
+  const percentage = (numberPacked / packedList) * 100;
   return (
     <footer className="h-[10vh] w-full flex items-center justify-center bg-black text-white px-4 py-8">
-      <p className="italic">
-        🧳 You have {packedList} items on your list, and you've packed
-        <span> {numberPacked}</span> (
-        {Math.floor((numberPacked / packedList) * 100)}%)
-      </p>
+      {percentage === 100 ? (
+        <p className="italic">You're ready to go 🚀</p>
+      ) : (
+        <p className="italic">
+          🧳 You have {packedList} items on your list, and you've packed
+          <span> {numberPacked}</span> ({Math.round(percentage)}%)
+        </p>
+      )}
     </footer>
   );
 };
